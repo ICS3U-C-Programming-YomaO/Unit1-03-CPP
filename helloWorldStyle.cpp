cba8b91722e7c33d2c482a5af96885fe0e7bd99d
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2025-09-14 22:49:49 +0000

    Hello world with style

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 Yoma Ozoh All rights reserved.
+//
+// Created by: Yoma Ozoh
+// Date: September 14, 2025
+// This program displays "Hello, world with style."
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "Woohooo, C++! Not hard at all" << std::endl;
+}
